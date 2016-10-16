ef8086d8c64f37bb44572c318d792fc313501752
nicmosc <nmoscholios@gmail.com>
2016-10-17 01:56:30 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask_assets import Environment, Bundle
+
+app = Flask(__name__)
+
+assets = Environment(app)
+less = Bundle('styles/app.less', filters='less', output='app.css')
+assets.register('less',less)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+
+# @app.route('/_add_numbers')
+# def addNumbers():
+#     a = request.args.get('a', 0, type=int)
+#     b = request.args.get('b', 0, type=int)
+#
+#     initRequest = requests.post("http://nlp.stanford.edu:8080/parser/index.jsp",
+#                                 timeout=1)  # test if the parser is still at this address
+#     status_code = initRequest.status_code
+#     print status_code
+#
+#     return jsonify(result=a + b)
+
+if __name__ == '__main__':
+  app.run(debug=True)
